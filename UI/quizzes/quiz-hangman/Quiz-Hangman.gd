4530d4cc8ca424dc559d37ad27ede779dd8b42f2
JCpac <joaoc.costa@outlook.pt>
2022-05-18 03:13:49 +0100

    Created logic for generating buttons with the target word's hidden letters and a few extra random letters
    Removed duplicate buttons from the scene

--- UI/quizzes/quiz-hangman/Quiz-Hangman.gd
+++ UI/quizzes/quiz-hangman/Quiz-Hangman.gd
@@ -2,26 +2,34 @@ extends PanelContainer
 
 
 # PRELOADS & CONSTS
+const MIN_NUM_ANSWERS = 2
+const MAX_NUM_ANSWERS = 10
 
 # SIGNALS
 signal correct
 signal wrong
+signal completed
 
 # EXPORTS
+export(int) var minNumColumns = 1
+export(int) var maxNumColumns = 4
 export(String) var debugTarget = "ball"
 export(int, 0, 10) var debugHintChars = 1
+export(int, 2, 10) var debugNumAnswers = 6
 
 # VARS
-onready var targetImage: TextureRect = $QuizArea/PanelContainer/HSplitContainer/Image/TextureRect
-onready var targetLabel: Label = $QuizArea/PanelContainer/HSplitContainer/Control/Label
-onready var answerLine: LineEdit = $QuizArea/PanelContainer/HSplitContainer/Control/LineEdit
+onready var targetImage: TextureRect = $QuizArea/HBoxContainer/LeftSide/Image/TextureRect
+onready var targetLabel: Label = $QuizArea/HBoxContainer/RightSide/VBoxContainer/Target
+onready var answerGrid: GridContainer = $QuizArea/HBoxContainer/RightSide/VBoxContainer/Answers
+onready var answerButton: Button = $QuizArea/HBoxContainer/RightSide/VBoxContainer/Answers/Answer1
 var target: String
+var rng: RandomNumberGenerator
 
 func _ready():
-	_setFocus()
-
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
 	if OS.is_debug_build() and get_parent() == get_tree().root:
-		prepareQuiz(debugTarget, debugHintChars)
+		prepareQuiz(debugTarget, debugHintChars, debugNumAnswers)
 
 func _gui_input(event):
 	if event is InputEventKey:
@@ -35,7 +43,7 @@ func _gui_input(event):
 
 				print_debug("Quiz Write Prompt was wrong")
 
-func prepareQuiz(targetWord: String, numOfHintChars: int) -> void:
+func prepareQuiz(targetWord: String, numOfHintChars: int, numOfAnswerButtons: int) -> void:
 	self.target = targetWord
 
 	# Set target image
@@ -49,11 +57,38 @@ func prepareQuiz(targetWord: String, numOfHintChars: int) -> void:
 		numOfHintChars = targetHalfLength
 	targetLabel.text = _generateTargetWithHintLetters(targetWord, numOfHintChars)
 
+	# Restrict `numOfAnswerButtons` to their assigned range and to even numbers
+	if numOfAnswerButtons & 1:
+		numOfAnswerButtons += 1
+	if numOfAnswerButtons < MIN_NUM_ANSWERS:
+		numOfAnswerButtons = MIN_NUM_ANSWERS
+	elif numOfAnswerButtons > MAX_NUM_ANSWERS:
+		numOfAnswerButtons = MAX_NUM_ANSWERS
+
+	# Get hint character indexes
+	var hintCharIndexes: Array = []
+	for i in targetWord.length():
+		if not targetLabel.text[i] == '_':
+			hintCharIndexes.append(i)
+
+	# Generate answer buttons and place them on the tree, while also removing the placeholder button
+	var buttonsArray: Array = _generateAnswerButtons(targetWord, numOfAnswerButtons, hintCharIndexes)
+	answerGrid.remove_child(answerButton)
+	answerButton.queue_free()
+	for node in buttonsArray:
+		answerGrid.add_child(node)
+
+	# Calculate new appropriate grid size (floored half of the number of answers)
+	var newNumOfColumns: int = numOfAnswerButtons >> 1
+	if newNumOfColumns < minNumColumns:
+		newNumOfColumns = minNumColumns
+	elif newNumOfColumns > maxNumColumns:
+		newNumOfColumns = maxNumColumns
+	answerGrid.columns = newNumOfColumns
+
 func _generateTargetWithHintLetters(targetWord: String, numOfHintChars: int) -> String:
 	var shownTargetWord: String = "_".repeat(targetWord.length())
 	var chosenHintCharsIndexes: Array = []
-	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-	rng.randomize()
 
 	while chosenHintCharsIndexes.size() < numOfHintChars:
 		# Choose character index that hasn't been chosen yet
@@ -67,8 +102,33 @@ func _generateTargetWithHintLetters(targetWord: String, numOfHintChars: int) -> 
 
 	return shownTargetWord
 
-func _setFocus() -> void:
-	answerLine.grab_focus()
+func _generateAnswerButtons(targetWord: String, numOfAnswerButtons: int, hintCharIndexes: Array) -> Array:
+	var answerChars: Array = []	# Holds the possible answers
+	var answerButtons: Array = []	# Holds the buttons with the possible answers
+
+	# Add target word's characters to possible answers, except for the hint characters
+	var tempTargetWord: Array = []
+	for i in range(0, targetWord.length()):
+		if not i in hintCharIndexes:
+			tempTargetWord.append(targetWord[i])
+	answerChars = tempTargetWord
+
+	# Randomize and add extra characters
+	var numOfExtraChars = numOfAnswerButtons - len(answerChars)
+	while numOfExtraChars:
+		var randomASCII: int = rng.randi_range(97, 122)	# ASCII values for range a-z
+		answerChars.append(char(randomASCII))
+		numOfExtraChars -= 1
+
+	# Shuffle answer characters array and create buttons
+	answerChars.shuffle()
+	for i in answerChars:
+		# Generate duplicate buttons and assign them values
+		var newButton: Button = answerButton.duplicate()
+		newButton.text = i
+		answerButtons.append(newButton)
+
+	return answerButtons
 
 func _validateAnswer() -> bool:
-	return answerLine.text == target
+	return true
