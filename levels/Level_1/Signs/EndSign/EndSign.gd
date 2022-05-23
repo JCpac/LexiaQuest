932f837c8e13530d5e6e3fb022319465825a9f19
JCpac <joaoc.costa@outlook.pt>
2022-05-23 17:30:49 +0100

    Expose `EndSign` scene's hitbox height, to facilitate its change
    The hitbox's shape is moved programatically to keep its bottom edge on the ground
    Increase `EndSign`'s height in `Level-1`

--- levels/Level_1/Signs/EndSign/EndSign.gd
+++ levels/Level_1/Signs/EndSign/EndSign.gd
@@ -4,8 +4,18 @@ class_name EndSign extends Node2D
 # SIGNALS
 signal playerReached
 
+# EXPORTS
+export(int, 0, 255, 8) var hitboxHeight: int = 32
+
+# METHODS
 func _ready():
-	pass
+	_setHitBoxShapeAndPosition()
+
+func _setHitBoxShapeAndPosition() -> void:
+	var hitbox: CollisionShape2D = $Area2D/CollisionShape2D
+	var hitboxShape: RectangleShape2D = hitbox.shape
+	hitboxShape.extents.y = hitboxHeight
+	hitbox.position.y = -hitboxShape.extents.y / 2
 
 func _on_Area2D_body_entered(_body):
 	emit_signal("playerReached")
