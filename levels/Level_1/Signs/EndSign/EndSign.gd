7a680536cb82419d53e85316999bce294e550a7d
JCpac <joaoc.costa@outlook.pt>
2022-05-24 03:59:26 +0100

    Fix wrong `EndSign` hitbox vertical extent calculation

--- levels/Level_1/Signs/EndSign/EndSign.gd
+++ levels/Level_1/Signs/EndSign/EndSign.gd
@@ -5,7 +5,7 @@ class_name EndSign extends Node2D
 signal playerReached
 
 # EXPORTS
-export(int, 0, 255, 8) var hitboxHeight: int = 32
+export(int, 0, 255, 8) var hitboxHeight: int = 64
 
 # METHODS
 func _ready():
@@ -14,7 +14,7 @@ func _ready():
 func _setHitBoxShapeAndPosition() -> void:
 	var hitbox: CollisionShape2D = $Area2D/CollisionShape2D
 	var hitboxShape: RectangleShape2D = hitbox.shape
-	hitboxShape.extents.y = hitboxHeight
+	hitboxShape.extents.y = hitboxHeight / 2
 	hitbox.position.y = -hitboxShape.extents.y / 2
 
 func _on_Area2D_body_entered(_body):
