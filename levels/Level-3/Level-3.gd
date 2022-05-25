b622819639372a611d095b8c9e41c018e0f8f26f
JCpac <joaoc.costa@outlook.pt>
2022-05-25 21:57:12 +0100

    Create `Level-3`'s platforming elements

---
+++ levels/Level-3/Level-3.gd
@@ -0,0 +1,25 @@
+extends Level
+
+
+# PRELOADS & CONSTS
+
+# EXPORTS
+
+# VARS
+onready var quiz = $"CanvasLayer/Quiz-FourChoices-Syllables"
+
+# METHODS
+func onPresentCollected() -> void:
+	# Open Syllables quiz
+	quiz.prepareQuiz(quiz.debugTarget, quiz.debugAnswers)
+	quiz.visible = true
+	player.paused = true
+
+func _on_Quiz_complete():
+	# Wait 2 seconds, add 1 to score and continue the game
+	quizCompleteTimer.start()
+	yield(quizCompleteTimer, "timeout")
+	score += 1
+	scoreCounter.setScore(score)
+	quiz.visible = false
+	player.paused = false
