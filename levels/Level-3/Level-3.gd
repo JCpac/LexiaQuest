d181c70d6241745639719bed1e78fadd895a761e
JCpac <joaoc.costa@outlook.pt>
2022-05-25 22:33:40 +0100

    Set `Quiz-Hangman` quiz scene as `Level-3`'s default quiz

--- levels/Level-3/Level-3.gd
+++ levels/Level-3/Level-3.gd
@@ -6,16 +6,16 @@ extends Level
 # EXPORTS
 
 # VARS
-onready var quiz = $"CanvasLayer/Quiz-FourChoices-Syllables"
+onready var quiz = $"CanvasLayer/Quiz-Hangman"
 
 # METHODS
 func onPresentCollected() -> void:
-	# Open Syllables quiz
-	quiz.prepareQuiz(quiz.debugTarget, quiz.debugAnswers)
+	# Open quiz
+	quiz.prepareQuiz(quiz.debugTarget, quiz.debugHintChars, quiz.debugNumAnswers)
 	quiz.visible = true
 	player.paused = true
 
-func _on_Quiz_complete():
+func _on_Quiz_completed():
 	# Wait 2 seconds, add 1 to score and continue the game
 	quizCompleteTimer.start()
 	yield(quizCompleteTimer, "timeout")
