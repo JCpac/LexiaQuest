9b8ca64dd29ca3526e93f8a78aa737a50c2d26de
JCpac <joaoc.costa@outlook.pt>
2022-05-29 16:18:39 +0100

    `StartScreenUI` now sets its buttons' cursor shape to a pointing hand only when the scene is enabled

--- UI/start-screen/StartScreenUI.gd
+++ UI/start-screen/StartScreenUI.gd
@@ -13,11 +13,14 @@ func _ready():
 
 func setEnabled(value: bool) -> void:
 	enabled = value
-	$Start/Button.disabled = not value
-	$Exit/Button.disabled = not value
 
-	if value:
-		$Start/Button.grab_focus()
+	var startButton: Button = $Start/Button
+	startButton.disabled = not value
+	startButton.mouse_default_cursor_shape = CURSOR_POINTING_HAND if value else CURSOR_ARROW
+
+	var exitButton: Button = $Exit/Button
+	exitButton.disabled = not value
+	exitButton.mouse_default_cursor_shape = CURSOR_POINTING_HAND if value else CURSOR_ARROW
 
 func _on_StartButton_pressed():
 	emit_signal("startGame")
