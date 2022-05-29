39362507ccc28ba26c225a8f96bdfdf748dc8e46
JCpac <joaoc.costa@outlook.pt>
2022-05-29 04:19:53 +0100

    Quiz uses `Button-Quiz` scene instead of `Button-SimpleWord`
    Adapt `prepareQuiz()` and its subsequent methods to use the values provided by `QuizGenerator`
    Some refactoring
    Started adapting quiz to work for both "Starts With" and "Rhymes With" quizzes:
    - created enumeration `SYLLABLES_QUIZ_TYPES`
    - `Explanation` label gets different content depending on the quiz type

--- UI/quizzes/syllables/Quiz-Syllables.gd
+++ UI/quizzes/syllables/Quiz-Syllables.gd
@@ -2,7 +2,7 @@ extends PanelContainer
 
 
 # PRELOADS & CONSTS
-const buttonWordScene: PackedScene = preload("res://UI/Button/Word/Button-Word.tscn")
+const buttonQuizScene: PackedScene = preload("res://UI/Button/Quiz/Button-Quiz.tscn")
 
 # SIGNALS
 signal correct
@@ -10,22 +10,31 @@ signal wrong
 signal completed
 
 # EXPORTS
+export(String) var startsWithText = "Seleciona as palavras que comecem com:"
+export(String) var rhymesWithText = "Seleciona as palavras que rimem com:"
 export(int) var minNumColumns = 1
 export(int) var maxNumColumns = 4
 export(String) var debugTarget = "fa"
-export(Array) var debugAnswers: Array = [
-	Word.new(["fa", "mi", "ly"], "family"),
-	Word.new(["fa", "vo", "rite"], "favorite"),
-	Word.new(["fa", "ther"], "father"),
-	Word.new(["lan", "guage"], "language"),
-	Word.new(["fea", "ther"], "feather"),
-	Word.new(["foot", "ball"], "football")
+export(Array) var debugCorrectAnswers: Array = [
+	"family",
+	"favourite",
+	"father"
+]
+export(Array) var debugWrongAnswers: Array = [
+	"language",
+	"feather",
+	"football"
 ]
 
+# ENUMS
+enum SYLLABLES_QUIZ_TYPES {STARTS_WITH, RHYMES_WITH}
+
 # VARS
+onready var explanationLabel: RichTextLabel = $QuizArea/VBoxContainer/Exercise/Explanation
 onready var targetLabel: RichTextLabel = $QuizArea/VBoxContainer/Exercise/Target
 onready var answerGrid: GridContainer = $QuizArea/VBoxContainer/Answers
 var numCorrectAnswersLeft: int
+var quizType
 
 func _init():
 	randomize()
@@ -33,17 +42,28 @@ func _init():
 func _ready():
 	# If the quiz scene itself is played, automatically use debug values
 	if OS.is_debug_build() and get_parent() == get_tree().root:
-		prepareQuiz(debugTarget, debugAnswers)
+		prepareQuiz(debugTarget, debugCorrectAnswers, debugWrongAnswers, SYLLABLES_QUIZ_TYPES.STARTS_WITH)
 
-# Assigns the quiz with a target syllable and several possible answers
+# Assigns the quiz with a target word part and several possible answers
 # Generates the quiz UI based on the provided values
 # Assumes that at least 1 of the provided answers is correct
-# Expects the elements of `answersArray` to be of type `Word`
-func prepareQuiz(targetSyllable: String, answersArray: Array) -> void:
-	answersArray.shuffle()
+# Expects the elements of `correctAnswers` and `wrongAnswers` to be of type `String`
+# `quizType` must be one of the values in the `SYLLABLES_QUIZ_TYPES` enumeration
+func prepareQuiz(targetWordPart: String, correctAnswers: Array, wrongAnswers: Array, quizType) -> void:
+	self.quizType = quizType
+	self.targetLabel.bbcode_text = "[center][b]%s[/b][/center]" % targetWordPart
+	self.numCorrectAnswersLeft = len(correctAnswers)
 
-	targetLabel.bbcode_text = "[center][b]%s[/b][/center]" % targetSyllable
-	self.numCorrectAnswersLeft = _countCorrectAnswers(targetSyllable, answersArray)
+	# Set explanation label's content
+	match quizType:
+		SYLLABLES_QUIZ_TYPES.STARTS_WITH:
+			self.explanationLabel.bbcode_text = "[center]%s[/center]" % self.startsWithText
+
+		SYLLABLES_QUIZ_TYPES.RHYMES_WITH:
+			self.explanationLabel.bbcode_text = "[center]%s[/center]" % self.rhymesWithText
+
+		_:
+			self.explanationLabel.bbcode_text = "[center]Algo correu mal ao gerar esta atividade[/center]"
 
 	# Clear answer grid
 	# (this quiz scene is instanced once per level and reused for each activity)
@@ -51,54 +71,54 @@ func prepareQuiz(targetSyllable: String, answersArray: Array) -> void:
 		gridChild.free()
 
 	# Generate answer buttons and place them on the tree
-	var buttonsArray: Array = _generateAnswerButtons(answersArray)
+	var buttonsArray: Array = _generateAnswerButtons(correctAnswers, wrongAnswers)
 	for node in buttonsArray:
 		answerGrid.add_child(node)
 
-	print_debug("Quiz Syllables: Quiz prepared with target syllable '%s'" % targetSyllable)
+	print_debug("Quiz Syllables: Quiz prepared with target syllable '%s'" % targetWordPart)
 
-# Count how many of the provided answers match the provided target syllable
-func _countCorrectAnswers(targetSyllable: String, answersArray: Array) -> int:
-	var count: int = 0
-	for answer in answersArray:
-		if targetSyllable == (answer as Word).syllables[0]:
-			count += 1
-	return count
-
-# Instance `Button-Word` scenes and assign them the provided `Word` instances
-func _generateAnswerButtons(answersArray: Array) -> Array:
+# Instance `Button-Quiz` scenes and assign them the provided answers
+func _generateAnswerButtons(correctAnswers: Array, wrongAnswers: Array) -> Array:
 	var answerButtons: Array = []
 
-	for answer in answersArray:
-		var newButton: Button = buttonWordScene.instance()
-		newButton.word = answer
-		newButton.text = newButton.word.word
+	for answer in correctAnswers:
+		var newButton: ButtonQuiz = buttonQuizScene.instance()
+		newButton.text = answer
+		newButton.isCorrect = true
 		newButton.connect("pressed", self, "_on_AnswerButton_pressed", [newButton])
 		answerButtons.append(newButton)
 
+	for answer in wrongAnswers:
+		var newButton: ButtonQuiz = buttonQuizScene.instance()
+		newButton.text = answer
+		newButton.isCorrect = false
+		newButton.connect("pressed", self, "_on_AnswerButton_pressed", [newButton])
+		answerButtons.append(newButton)
+
+	answerButtons.shuffle()
 	return answerButtons
 
-func _validateAnswer(answer: Word) -> bool:
-	return answer.syllables[0] == targetLabel.text
+func _validateAnswer(target: ButtonQuiz) -> bool:
+	return target.isCorrect
 
-func _on_AnswerButton_pressed(target: Button) -> void:
-	if _validateAnswer(target.word):
+func _on_AnswerButton_pressed(target: ButtonQuiz) -> void:
+	target.revealState(true)
+
+	if _validateAnswer(target):
 		numCorrectAnswersLeft -= 1
-		target.setCorrect()
 		emit_signal("correct")
-		print_debug("Quiz Syllables: Answer '%s' was correct" % target.word.word)
+		print_debug("Quiz Syllables: Answer '%s' was correct" % target.text)
 
 		if not numCorrectAnswersLeft:
 			_revealRemainingAnswers()
 			emit_signal("completed")
 			print_debug("Quiz Syllables: Quiz complete")
 	else:
-		target.setWrong(true)
 		emit_signal("wrong")
-		print_debug("Quiz Syllables: Answer '%s' was wrong" % target.word.word)
+		print_debug("Quiz Syllables: Answer '%s' was wrong" % target.text)
 
 # Reveals the unchosen wrong answers after quiz completion
 func _revealRemainingAnswers() -> void:
-	for buttonWord in answerGrid.get_children():
-		if not buttonWord.disabled and not buttonWord.word.syllables[0] == targetLabel.text:
-			buttonWord.setWrong(false)
+	for button in answerGrid.get_children():
+		if not button.disabled:
+			button.revealState(false)
